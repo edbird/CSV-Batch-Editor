e3d90750cd2b8591628ad2086c6b8d96ae376e5f
birdsale <birdsale@idebirdsale1.london.torstone.local>
2022-06-15 12:38:11 +0100

    Added additional files

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-./a.out --script-file input-script.txt
+#./a.out --script-file input-script.txt
+
+#./a.out --script-file input-script-gideon.txt
+./a.out --script-file input-script-gideon-groupabletrialbalance.txt
